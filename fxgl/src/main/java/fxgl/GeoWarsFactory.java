8e876f36357201320ab086df4bbfc59873172649
Nathaniel Arrowood <nathanielarrowood@gmail.com>
2023-02-02 12:30:02 -0500

    Initial branch created.

---
+++ fxgl/src/main/java/fxgl/GeoWarsFactory.java
@@ -0,0 +1,53 @@
+package fxgl;
+
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
+import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getInput;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
+
+import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.dsl.components.RandomMoveComponent;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+
+import fxgl.GeoWarsApp.EntityType;
+import com.almasb.*;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class GeoWarsFactory implements EntityFactory {
+
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        return entityBuilder()
+                .from(data)
+                .type(EntityType.PLAYER)
+                .viewWithBBox(new Rectangle(30, 30, Color.BLUE))
+                .collidable()
+                .build();
+    }
+    
+    @Spawns("bullet")
+    public Entity newBullet(SpawnData data) {
+        Entity player = getGameWorld().getSingleton(EntityType.PLAYER);
+        Point2D direction = getInput().getMousePositionWorld()
+            .subtract(player.getCenter());
+
+        return entityBuilder()
+                .from(data)
+                .type(EntityType.BULLET)
+                .viewWithBBox(new Rectangle(10, 2, Color.BLACK))
+                .collidable()
+                .with(new ProjectileComponent(direction, 1000))
+                .with(new OffscreenCleanComponent())
+                .build();
+    }
+    
+}
